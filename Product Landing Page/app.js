67bea5d5ba345e56b7e9db4316ce32cf5d483321
HarkLikeButter <92184835+HardLikeButter@users.noreply.github.com>
2022-03-30 16:36:00 -0400

    Updates

---
+++ Product Landing Page/app.js
@@ -0,0 +1,14 @@
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelectorAll(".nav-link");
+
+navToggle.addEventListener("click", () => {
+
+    document.body.classList.toggle("nav-open");
+
+})
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        document.body.classList.remove("nav-open");
+    })
+})
