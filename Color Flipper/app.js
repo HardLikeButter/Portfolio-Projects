8ef7c7b6fda9bf90087209a3618ea051e0fb404f
HarkLikeButter <92184835+HardLikeButter@users.noreply.github.com>
2022-03-30 17:48:15 -0400

    Color Flipper Upload

---
+++ Color Flipper/app.js
@@ -0,0 +1,18 @@
+const colorArr = ["green", "red", "Rgba(133, 122, 200)", "#f15025", "#00FFFF", "rgb(255, 192, 203)", "yellow", "white", "black", "brown"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+
+
+btn.addEventListener("click", () => {
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+
+    color.textContent = colorArr[randomNumber];
+    document.body.style.backgroundColor = colorArr[getRandomNumber()];
+})
+
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colorArr.length);
+}
